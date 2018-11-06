0250f888fa26069200212f1ea52a857421c9e738
yezixigua <541889983@qq.com>
2018-11-06 15:58:27 +0800

    add config files

---
+++ config/run.sh
@@ -0,0 +1,10 @@
+
+
+# 建立对应软件的配置软连接，将配置源文件放在项目本地，在对应软件目录下生成链接，链接到本地
+ln -s /home/an/gitcode/mySite/config/supervisor.conf /etc/supervisor/conf.d/mySite.conf
+ln -s /home/an/gitcode/mySite/config/nginx_conf /etc/nginx/sites-enabled/mySite
+
+# 重启服务
+sudo service nginx restart
+sudo service supervisor restart
+
