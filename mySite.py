97f41945df41de9feaf24534f91fecd334ae2cc1
yezixigua <541889983@qq.com>
2018-11-13 15:12:57 +0800

    add ip

--- mySite.py
+++ mySite.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='')
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    ip = request.remote_addr
+    return render_template('index.html', ip=ip)
 
 
 if __name__ == '__main__':
 
     config = {
-        'host' : '0.0.0.0',
-        'port' : 3000,
+        'host': '0.0.0.0',
+        'port': 3000,
         'debug': True,
     }
     app.run(**config)
