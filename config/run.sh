0add2f2a7f480438b5bf0197397409a223bbeb4a
yezixigua <541889983@qq.com>
2020-04-20 17:34:44 +0800

    modify config

--- config/run.sh
+++ config/run.sh
@@ -13,8 +13,8 @@ rm /etc/supervisor/conf.d/mySite.conf
 rm /etc/nginx/sites-enabled/mySite
 
 # 建立对应软件的配置软连接，将配置源文件放在项目本地，在对应软件目录下生成链接，链接到本地
-ln -s /home/an/gitcode/mySite/config/supervisor.conf /etc/supervisor/conf.d/mySite.conf
-ln -s /home/an/gitcode/mySite/config/nginx_config /etc/nginx/sites-enabled/mySite
+ln -s /home/an/space/mySite/config/supervisor.conf /etc/supervisor/conf.d/mySite.conf
+ln -s /home/an/space/mySite/config/nginx_config /etc/nginx/sites-enabled/mySite
 
 # 重启服务
 service supervisor restart
