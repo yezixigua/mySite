78779f1a1dc57c6d17fb22415519927963231fe7
yezixigua <541889983@qq.com>
2020-04-20 15:53:27 +0800

    back to http

--- mySite.py
+++ mySite.py
@@ -21,13 +21,27 @@ def hello_world():
     return render_template('index_android.html', ip=ip)
 
 
+@app.route('/python')
+def python_page():
+    # 本地没有配置nginx代理，本地调试的时候选用上面的
+    # ip = request.remote_addr
+
+    print(type(request))
+    if 'X-Forwarded-For' in request.headers:
+        ip = request.headers['X-Forwarded-For']
+    else:
+        ip = '6.6.6.6'
+    db.add_data(ip)
+    return render_template('index_python.html', ip=ip)
+
+
 if __name__ == '__main__':
 
     config = {
         'host': '0.0.0.0',
-        'port': 443,
+        'port': 80,
         'debug': True,
-        'ssl_context': ('cer/1_www.yezixigua.cn_bundle.crt', 'cer/2_www.yezixigua.cn.key'),
+        # 'ssl_context': ('cer/1_www.yezixigua.cn_bundle.crt', 'cer/2_www.yezixigua.cn.key'),
     }
     # db.init_data()
     app.run(**config)
