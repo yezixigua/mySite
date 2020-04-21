e71259b76867e04244806ef0ee9e95bc89a12c41
yezixigua <541889983@qq.com>
2020-04-21 14:16:39 +0800

    modify config

--- config/run.sh
+++ config/run.sh
@@ -11,10 +11,12 @@ service nginx stop
 # 删除原本建立的软连接
 rm /etc/supervisor/conf.d/mySite.conf
 rm /etc/nginx/sites-enabled/mySite
+rm /etc/nginx/conf.d/mySite.conf
 
 # 建立对应软件的配置软连接，将配置源文件放在项目本地，在对应软件目录下生成链接，链接到本地
 ln -s /home/an/space/mySite/config/supervisor.conf /etc/supervisor/conf.d/mySite.conf
 ln -s /home/an/space/mySite/config/nginx_config /etc/nginx/sites-enabled/mySite
+ln -s /home/an/space/mySite/config/nginx_config /etc/nginx/conf.d/mySite.conf
 
 # 重启服务
 #supervisorctl start mySite
